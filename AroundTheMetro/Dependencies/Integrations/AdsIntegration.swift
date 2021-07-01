ad69cbf155daaf07e9597fcb60553b9477ba58dd
Fahad Baig <fahadbaig.ioss@gmail.com>
2021-07-01 16:08:02 +0500

    Removed interstitial ads after premium purchase.

--- AroundTheMetro/Dependencies/Integrations/AdsIntegration.swift
+++ AroundTheMetro/Dependencies/Integrations/AdsIntegration.swift
@@ -82,7 +82,9 @@ extension AdsIntegration: GADBannerViewDelegate {
 extension AdsIntegration: GADInterstitialDelegate {
     func interstitialDidReceiveAd(_ ad: GADInterstitial) {
         if let adsContainer = adsContainer {
+            if !IAPManager.shared.isSubscribed {
             ad.present(fromRootViewController: adsContainer)
+            }
         }
     }
 }
